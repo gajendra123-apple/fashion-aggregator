9451a9c56b256e0f641a647bcbfbfe3f1045769c
Janak lodhi <janakthakur346@gmail.com>
2024-05-09 13:01:11 +0530

    fix active admin

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+<<<<<<< Updated upstream
   has_many :subcategories, class_name: "Category", foreign_key: "parent_category", dependent: :destroy
   belongs_to :category_id, class_name: "Category", optional: true
   has_many :products, dependent: :destroy
@@ -7,3 +8,10 @@ class Category < ApplicationRecord
     ["created_at", "id", "id_value", "name", "updated_at"]
   end
 end
+=======
+
+	def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "name", "updated_at"]
+  end
+end
+>>>>>>> Stashed changes
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+<<<<<<< Updated upstream
     belongs_to :category
     belongs_to :sub_category
     has_many :order_items, dependent: :destroy
@@ -9,3 +10,10 @@ class Product < ApplicationRecord
       ["created_at", "description", "id", "id_value", "image_url", "name", "price", "stock_quantity", "updated_at"]
     end
 end   
+=======
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "id_value", "image_url", "name", "price", "stock_quantity", "updated_at"]
+  end
+end 
+>>>>>>> Stashed changes
