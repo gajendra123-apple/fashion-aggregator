828feb1f35c2f0964c780214c90c44a6d202e3ff
Janak lodhi <janakthakur346@gmail.com>
2024-05-27 18:40:02 +0530

    resolved conflicts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,52 +1,52 @@
 class CartsController < ApplicationController
   include Authentication
   before_action :authenticate_user!
-  before_action :set_cart, only: [:show, :add_product, :remove_product, :apply_coupon, :checkout]
+  before_action :set_cart, only: [:show, :add_product, :remove_product, :apply_coupon]
 
   def show
-    render json: @cart, include: :cart_items
+    @cart.recalculate_total_with_coupon
+    render json: @cart, serializer: CartSerializer, status: :ok
   end
 
   def add_product
-    @cart.add_product(params[:product_id], params[:quantity].to_i)
-    if @cart.save
-      render json: { message: "Product added to cart successfully", cart: @cart }, status: :ok
+    product_id = params[:product_id].to_i
+    quantity = params[:quantity].presence || 1
+    @cart.add_product(product_id, quantity)
+    if @cart.save       
+      cart_data = ActiveModelSerializers::SerializableResource.new(@cart, serializer: CartSerializer).as_json
+      render json: cart_data.merge(message: "Product added to cart successfully"), status: :ok 
     else
       render json: @cart.errors, status: :unprocessable_entity
-      product_id = params[:product_id].to_i
-      quantity = params[:quantity].to_i
-      if @cart.cart_items.exists?(product_id: product_id)
-        render json: { message: "This product is already in the cart. Please select another one." }, status: :unprocessable_entity
-      else
-        @cart.add_product(product_id, quantity)
-        if @cart.save
-          render json: { message: "Product added to cart successfully", cart: @cart }, status: :ok
-        else
-          
-          render json: @cart.errors, status: :unprocessable_entity
-        end
-      end
     end
   end
   
   def remove_product
-    product_removed = @cart.remove_product(params[:product_id])
-    if product_removed
-      render json: { message: "Product has been removed from the cart" }, status: :ok
+    cart_item_id = params[:cart_item_id].to_i
+    cart_item = current_user.cart.cart_items.find_by(id: cart_item_id)
+    if cart_item
+      cart_item.quantity -= 1
+      if cart_item.quantity <= 0
+        cart_item.destroy
+      else
+        cart_item.save
+      end
+      @cart.recalculate_total_with_coupon
+      render json: { message: "Product has been removed from the cart", total_price: @cart.total_price }, status: :ok
+
     else
       render json: { error: "Product not found in the cart" }, status: :not_found
     end
   end
 
-  def checkout
-    order = current_user.orders.build
-    if order.create_from_cart(@cart)
-      render json: { message: "Checkout successful", order: order }, status: :ok
+  def apply_coupon
+    if @cart.apply_coupon(params[:coupon_code])
+      render json: { message: "Coupon applied successfully", cart: @cart }, status: :ok
     else
-      render json: order.errors, status: :unprocessable_entity
+      render json: {error: "coupon not found"}, status: :unprocessable_entity
+      # render json: { error: @cart.errors.full_messages.join(', ') }, status: :unprocessable_entity
     end
   end
-
+  
   private
 
   def authenticate_user!
@@ -57,5 +57,9 @@ class CartsController < ApplicationController
 
   def set_cart
     @cart = current_user.cart || current_user.create_cart
+  end
+
+  def recalculate_total_with_coupon
+    total_without_coupon = @cart.cart_items.joins(:product).sum('cart_items.quantity * products.price')
   end
 end
