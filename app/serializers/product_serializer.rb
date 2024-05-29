c0c1dd7bf1f0ae389fd69793953645351d3bdf53
Janak lodhi <janakthakur346@gmail.com>
2024-05-29 17:52:58 +0530

    fixed dev server error for url_image

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -5,7 +5,7 @@ class ProductSerializer < ActiveModel::Serializer
   def image_url
     if object.image.attached?
       final_image = object.image
-      Rails.env.development? || Rails.env.test? || Rails.env.prduction ? Rails.application.routes.url_helpers.rails_blob_path(final_image, only_path: true) : final_image&.service_url&.split('?')&.first if final_image.attached?
+      Rails.env.development? || Rails.env.test? || Rails.env.prduction? ? Rails.application.routes.url_helpers.rails_blob_path(final_image, only_path: true) : final_image&.service_url&.split('?')&.first if final_image.attached?
     end
   end
 end
