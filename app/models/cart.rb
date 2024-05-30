3e4a1ace4299f18edb42f4b5ad0fd4011a6aa1d9
Janak lodhi <janakthakur346@gmail.com>
2024-05-30 13:22:04 +0530

    fixed the cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,14 +4,7 @@ class Cart < ApplicationRecord
   has_many :products, through: :cart_items
   has_many :products, dependent: :destroy
   has_many :orders, dependent: :destroy
-  # has_many :products, dependent: :destroy
-  belongs_to :coupon, optional: true
-  has_many :orders
-
-
-  # def total_price_without_coupon
-  #   cart_items.joins(:product).sum('cart_items.quantity * products.price')
-  # end
+  has_many :orders, dependent: :destroy
 
   def add_product(product_id, quantity = 1)
     current_item = cart_items.find_or_initialize_by(product_id: product_id)
@@ -19,17 +12,7 @@ class Cart < ApplicationRecord
     current_item.save
     recalculate_total_with_coupon
   end
-
-  # def recalculate_total_with_coupon
-  #   total_without_coupon = cart_items.joins(:product).sum('cart_items.quantity * products.price')
-  #   if coupon.present? && coupon.discount_percentage.present?
-  #     discount_amount = total_without_coupon * (coupon.discount_percentage / 100.0)
-  #     self.total_price = total_without_coupon - discount_amount
-  #   else
-  #     self.total_price = total_without_coupon
-  #   end
-  # end
-
+  
   def recalculate_total_with_coupon
     self.total_price = cart_items.includes(:product).sum do |item|
       item.product.price * item.quantity
