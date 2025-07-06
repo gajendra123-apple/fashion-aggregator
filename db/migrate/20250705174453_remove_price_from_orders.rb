70b381653471f914ae8ae6bf49fc0a708daf0745
gajendra123-apple <gajendrajput1011@gmail.com>
2025-07-06 12:08:20 +0530

    change migration

--- db/migrate/20250705174453_remove_price_from_orders.rb
+++ db/migrate/20250705174453_remove_price_from_orders.rb
@@ -1,5 +1,9 @@
 class RemovePriceFromOrders < ActiveRecord::Migration[7.1]
   def change
-    remove_column :orders, :price, :float
-  end
+    if column_exists?(:orders, :price)
+      remove_column :orders, :price
+      puts "✅ Removed column :price from orders"
+    else
+      puts "🛑 Skipping removal: 'price' column not found in 'orders'"
+    end  end
 end
